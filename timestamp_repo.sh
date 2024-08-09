f5dc9e345fa1006ba649553777b25e9b2d09034e
robert schultheis <robert.schultheis@gmail.com>
2024-08-08 22:35:50 -0600

    timestamp script uses find implementation

--- timestamp_repo.sh
+++ timestamp_repo.sh
@@ -2,6 +2,8 @@
 
 set -e
 
+LC_ALL=POSIX
+
 # Parse the command line
 if [ "$#" -ne 1 ]; then
   echo "usage: $0 <git remote>"
@@ -10,29 +12,17 @@ fi
 git_repo=$1
 
 local_clone=$(mktemp -d -t $(basename $0))
-echo $local_clone
 
 git clone $git_repo $local_clone
 
-bundle_file=$(mktemp -t $(basename $0))
 pushd $local_clone
 branch=$(git branch --show-current)
 sha=$(git rev-parse HEAD)
-git bundle create $bundle_file $branch
+repo_digest=$(find -s . -type f -not -path "*/.git/*" -print0 | xargs -0 sha256sum | sha256sum | cut -f1 -d ' ')
 popd
-
-echo "---"
-echo $bundle_file
-echo "---"
-
-digest=$(shasum -a 256 $bundle_file  | cut -f 1 -d ' ')
-
 rm -rf $local_clone
-# rm $bundle_file
 
-git_version=$(git --version)
-
-ext_json="{\"git_version\": \"$git_version\", \"repo\": \"$git_repo\", \"sha\":\"$sha\"}"
+ext_json="{\"repo\": \"$git_repo\", \"sha\":\"$sha\", \"branch\": \"$branch\"}"
 
 echo "Extended timestamp data:"
 echo $ext_json | jq
@@ -40,7 +30,7 @@ echo $ext_json | jq
 echo "Performing timestamp create"
 curl \
   --data-urlencode algorithm=sha256 \
-  --data-urlencode digest=$digest \
+  --data-urlencode digest=$repo_digest \
   --data-urlencode ext="$ext_json" \
   --user public:publicpublic \
   -o "$sha.tt" \
